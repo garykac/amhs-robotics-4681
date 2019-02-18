c23548da530a701cca6068251ccac26d334a6a57
cmarley3-14 <46498319+cmarley3-14@users.noreply.github.com>
2019-02-17 21:28:40 -0800

    adapting changes
    took out `int progress`, `boolean completed`, and added function `StraightenLegs()`

--- workspace/2019 DeepSpace/src/main/java/frc/robot/Walker.java
+++ workspace/2019 DeepSpace/src/main/java/frc/robot/Walker.java
@@ -6,8 +6,6 @@ package frc.robot;
  * Manages the walking of the robot for 2019.
  */
 public class Walker {
-    private int progress = 0;
-    private boolean completed = false;
     private WalkerLegs m_legs;
 
     public void WalkerInit() {
@@ -18,45 +16,25 @@ public class Walker {
     public void RaiseRobot() {
         m_legs.ExtendFront(true);
         m_legs.ExtendBack(true);
-        progress = 1;
         System.out.println("Raise Bot");
     }
 
     public void Walk() {
         m_legs.Walk(true);
-        progress = 2;
         System.out.println("Walk");
     }
+    
+    public void StraightenLegs() {
+        m_legs.Walk(false);
+        System.out.println("Un-Walk");
 
     public void RetractFrontLegs() {
         m_legs.ExtendFront(false);
         System.out.println("Retract Front Legs");
-        progress = 3;
     }
 
     public void RetractBackLegs() {
         m_legs.ExtendBack(false);
-        progress = 0;
-        completed = true;
         System.out.println("Retract Back Legs");
-    }
-
-    // This function has been deprecated because it's too idealized
-    // The changes have been adapted in Robot.java
-    public void Climb() {
-        if (completed == false){
-            if (progress == 3) {
-                RetractBackLegs();
-            }
-            else if (progress == 2) {
-                RetractFrontLegs();
-            } 
-            else if (progress == 1) {
-                Walk();
-            } 
-            else if (progress == 0) {
-                RaiseRobot();
-            }  
-        }
     }
 }
