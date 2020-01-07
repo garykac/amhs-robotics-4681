a6f6da901cbb94cb1f60584a1855fe5921a3f6f4
Team4681 <amhsrobotics4681@gmail.com>
2020-01-06 16:18:07 -0800

    re-update wheelchair

--- workspace/ProgrammingIntro/src/main/java/frc/robot/Robot.java
+++ workspace/ProgrammingIntro/src/main/java/frc/robot/Robot.java
@@ -9,6 +9,7 @@ public class Robot extends TimedRobot {
     Victor mo;
     Victor m1;
     Joystick controller;
+    double kSpeed;
     
     @Override
     public void robotInit(){
@@ -19,12 +20,23 @@ public class Robot extends TimedRobot {
 
     @Override
     public void teleopInit(){
+        kSpeed = 0.6;
     }
 
     @Override
     public void teleopPeriodic() {
         //xbox a b x y
-        mo.set(-controller.getRawAxis(1));
-        m1.set(controller.getRawAxis(5));
+        mo.set(-kSpeed*controller.getRawAxis(1));
+        m1.set(kSpeed*controller.getRawAxis(5));
+        if (controller.getRawButtonPressed(4)) {
+            if (kSpeed < 1)
+                kSpeed += 0.2;
+            System.out.println(kSpeed);
+        }
+        if (controller.getRawButtonPressed(1)) {
+            if (kSpeed > 0)
+            kSpeed -= 0.2;
+        System.out.println(kSpeed);
+        }
     }
 }
